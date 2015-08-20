e1f6c390e847630979cd4bc64963b6b264b1af10
leonlipu <lipusipupara@gmail.com>
2015-08-20 23:38:53 +0530

    try expect and finally lock

--- funda2.py
+++ funda2.py
@@ -6,3 +6,8 @@ print list(enumerate(favlist))
 #for loop with enumeration
 for i,fav in enumerate(favlist):
     print ("%d : %s" %(i,fav))
+#for
+
+letters =[len(n) for n in favlist]
+
+print letters
---
+++ trycatch.py
@@ -0,0 +1,12 @@
+try:
+    print "something is raised "
+    #raise Exception("ami")
+    print "is line will not executed"
+
+# print"this expection has been cought "
+except :
+        print "this expection is nothing "
+
+finally:
+
+        print "finally block which rrun ru  "
