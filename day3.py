2bdaf0a248ffca7ce71cb0300e62e57f9d04b8c3
leonlipu <lipusipupara@gmail.com>
2015-08-23 19:55:21 +0530

    class use has beed extended

--- day3.py
+++ day3.py
@@ -9,7 +9,19 @@ class abc:
 # this is method need to implimeted
   def get (self):
    print self.x
+   print "class abc is included"
 
 #object creation
 ob =abc(88,99);
 ob.get();
+
+class xyz :
+    def get(self):
+        print "class xyz is included"
+
+print "*****************declaring object for multiple class*****************"
+
+combinedObject= [abc(77,66),xyz()]
+
+for ob in combinedObject:
+    print ob.get()
