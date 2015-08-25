870ca19ce4d6574253b9aaa46399ba8b768f475c
git <lipusipupara@gmail.com>
2015-08-25 23:57:30 +0530

    json path implemented

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # python-learning
 
-it day to day learning on python topic 
+it day to day learning on python topic
+
+##installation before run these programs
+pip install jsonpath-rw
--- api.py
+++ api.py
@@ -1,14 +1,24 @@
 
 from urllib2 import urlopen
 import json
+from jsonpath_rw import jsonpath, parse
+
+
 class client:
+
     def getresponse(self):
-        url ="https://api.myjson.com/bins/2xqto"
-        response=urlopen(url)
-        data= response.read()
-        print data
+        url = "https://api.myjson.com/bins/1leee"
+        response = urlopen(url)
+        data = response.read()
+        jsonexpression =parse("$..glossary")
+        dataarray=[match.value for match in jsonexpression.find(data)]
+        #dataarray=jsonexpression.find(data);
+        #print dataarray
+        dataparse=[match.value for match in parse('foo[*].id').find({'foo': [{'id': 'bizzle'}, {'baz': 3}]})]
+        print dataparse
+
+        #print data
 
 
-
-ob = client();
-ob.getresponse();
+ob = client()
+ob.getresponse()
