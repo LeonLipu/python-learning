db984507ca3b884516d6b6f692f008fa27c82a5c
leonlipu <lipusipupara@gmail.com>
2015-09-10 01:16:08 +0530

    some enhancement for regular expression

--- day2.py
+++ day2.py
@@ -1,12 +1,20 @@
 import re
 
-line =" there is my mail box is there "
+line ="once you have accomplished small thing succefully and you may start greatone succufully"
 
 obj = re.match(r'.*',line, re.I|re.M)
 print obj.group()
 
 
-def concat():
-    print "this is function"
-    pass
-concat();
+
+print re.findall(r"\bs[\w]*",line)
+it= re.finditer(r"\bs[\w]*",line)
+
+print "**************************************************"
+for match in it:
+    print "{g} was found between in the indices {s}".format(g=match.group(),s=match.span())
+
+
+
+print "*****************************************"
+print re.split("\W+",line);
