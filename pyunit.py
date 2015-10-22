724225054f3e3c093361181fdb7c6bf42d152a38
leonlipu <lipusipupara@gmail.com>
2015-10-22 23:47:26 +0530

    main method has been added

---
+++ pyunit.py
@@ -0,0 +1,9 @@
+
+
+def writesomething(self):
+    print "something may lead to have into those parameter "
+def main():
+    print "many technology is comming here "
+
+if __name__=="__main__":
+    main()
