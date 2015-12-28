3ee7138ce1e19f589f025afde0f85119d2565d52
leonlipu <lipusipupara@gmail.com>
2015-12-28 23:58:00 +0530

    list has been added

---
+++ list.py
@@ -0,0 +1,14 @@
+#here is list tutorial of list
+
+# it is similar to array list
+
+list =["lipu","is","handsomeguy","and","he","not","solution","something"]
+
+print list
+
+print "****** accessing value ********";
+print "list[0] is ",list[0]
+print "list[1:3]",list[1:3]
+print "*************deleting *********** "
+del list[0]
+print "after deleting list[0] is ", list
