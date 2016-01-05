8da125f343a8a634bf34236e9c44416fab1b9a30
leonlipu <lipusipupara@gmail.com>
2016-01-05 23:26:17 +0530

    touple and dic are implimented

---
+++ dictionary.py
@@ -0,0 +1,34 @@
+#dictionary nothing but key-value pair and keys should be immutable object and can not modified
+dict ={'name':'brahmanda','age':'27'}
+print dict['name'],dict['age']
+dict['add']='kolkota' #adding new key
+print dict
+dict.clear()
+print "after clear" ,dict
+
+dic ={'name':"gandhi",'age':77}
+print "********length"
+print len(dic)
+print str(dic)
+print "********type "
+print type(dic) #return type of variable that passed into it
+dic2=dic.copy()
+print dic2
+print "********forming dic from tuple "
+sq=('p1','p2')
+dic3=dict.fromkeys(sq,'something')
+print dic3
+print "********geting value "
+print dic.get("sex",'male')
+print "******** has key"
+print dic.has_key('sex')
+print "*********** items "
+print dic.items()
+print "*********** keys  "
+print dic.keys()
+print "set default ",dic.setdefault('age',None)
+print "set default ",dic.setdefault('height ',None)
+dic2 ={'add':'kol'}
+dic.update(dic2)
+print "after update ", dic
+print "getting value " ,dic.values()
---
+++ touple.py
@@ -0,0 +1,17 @@
+# tuple always within () list [] and dic {}
+
+#tuple are immutable object
+
+topl1=("hi","bye","brahma")
+print topl1[0:2]
+#since it is immutable hence we can not change it value once it is declared
+
+#there is crim formula  c -concatenatinon r - repitation i -iteration m-membership
+print (1,2,3,4,5)+(4,3,5,6)
+print ("si",)*8 #here we have to give comma
+print ("si") in ("si","so","ani")
+for x in (1,2,3):print x
+print "length",len((1,2,3))
+print "max ", max((1,2,39))
+print "min ", min((1,2,39))
+print "convert list into tuple ",tuple([2,3,4])
