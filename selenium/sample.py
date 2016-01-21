69677ea41b612801bd112d2af545231c09c1d249
git <lipusipupara@gmail.com>
2016-01-21 22:44:23 +0530

    page title been added

--- selenium/sample.py
+++ selenium/sample.py
@@ -10,12 +10,12 @@ class PythonOrgSearch(unittest.TestCase):
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("http://www.python.org")
+        print driver.title
         self.assertIn("Python", driver.title)
         elem = driver.find_element_by_name("q")
         elem.send_keys("pycon")
         elem.send_keys(Keys.RETURN)
         assert "No results found." not in driver.page_source
-
 
     def tearDown(self):
         self.driver.close()
