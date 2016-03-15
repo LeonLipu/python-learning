43eef54e53678b1502800703bc871d3fff9022bf
leonlipu <lipusipupara@gmail.com>
2016-03-15 23:53:34 +0530

    fork

--- setmappling.py
+++ setmappling.py
@@ -1,1 +1,25 @@
+import os
+print "hello warld "
 
+def name():
+    print "myname "
+
+def child():
+    print "something"
+
+class abc:
+    def getname(self):
+        print "getnamefunction"
+    def setname(self,name):
+        print "this is setname"+name
+        print os.getpid()
+        pid=os.fork()
+        if pid==0:
+            child()
+        else :
+            print "parent process is running "
+
+
+ob=abc()
+ob.getname()
+ob.setname("lipu")
