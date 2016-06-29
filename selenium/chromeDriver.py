7bf0fdb95bab9ab81f8c3467102bf0235bbaf000
brahmanndak <lipusipupara@gmail.com>
2016-06-30 00:16:03 +0530

    fixed chrome driver

--- selenium/chromeDriver.py
+++ selenium/chromeDriver.py
@@ -1,8 +1,18 @@
 import os
 from selenium import webdriver
 
-ChromeDriver ='/Users/brahmanndak/Desktop/personalgit/python-learning/selenium'
+ChromeDriver ='/Users/brahmanndak/Desktop/personalgit/python-learning/selenium/chromedriver'
 os.environ["webdriver.chrome.driver"]=ChromeDriver
 driver=webdriver.Chrome(ChromeDriver)
+driver.get("http://www.google.com")
+driver.close()
 
-driver.get("npthong m,pr ethank it happen s")
+
+# import os
+# from selenium import webdriver
+#
+# chromedriver = "/Users/adam/Downloads/chromedriver"
+# os.environ["webdriver.chrome.driver"] = chromedriver
+# driver = webdriver.Chrome(chromedriver)
+# driver.get("http://stackoverflow.com")
+# driver.quit()
