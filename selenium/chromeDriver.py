5c5e1f43880d22817be205e9283d412cd2be6fd9
brahmanndak <lipusipupara@gmail.com>
2016-07-08 23:51:14 +0530

    added solution to inject javascript

--- selenium/chromeDriver.py
+++ selenium/chromeDriver.py
@@ -5,9 +5,12 @@ ChromeDriver ='/Users/brahmanndak/Desktop/personalgit/python-learning/selenium/c
 os.environ["webdriver.chrome.driver"]=ChromeDriver
 driver=webdriver.Chrome(ChromeDriver)
 driver.get("http://www.google.com")
+
+print driver.current_url+"this is pthon tutorial spource "
+print driver.execute_async_script("document.title"); # injecting javascript
+driver.get_screenshot_as_png("base.png");
 driver.close()
 
-print driver.current_url+"this is pthon tutorial spource "       
 
 
 
