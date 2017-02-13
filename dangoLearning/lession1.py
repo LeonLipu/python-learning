e8df4ce97b97928288cf47e856f2d0fa3afcd7fa
LeonLipu <lipusipupara@gmail.com>
2017-02-14 02:17:37 +0530

    setter and getter file added

---
+++ dangoLearning/lession1.py
@@ -0,0 +1,16 @@
+class abc:
+
+    def __init__(self):
+        print "nothing but which need to have "
+    def ram(self):
+        print "nothing to be worry"
+    def setvalue(self):
+        self.name="lipu"
+
+    def getvalue(self):
+        print self.name
+
+ob=abc()
+ob.ram()
+ob.setvalue()
+ob.getvalue()
